d3cbff0500aa6d58105ee3942c52c16745e24cce
Coleton Watt <electricninja102@gmail.com>
2021-10-25 13:00:54 -0600

    Initial Commit

---
+++ Unit 7 No Brainer: Stack/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Unit 7 No Brainer: Stack
+//
+//  Created by Coleton Watt on 10/25/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
